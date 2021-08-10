e2a3080fd5b5bf045a093ad6b4adb86edda92464
oddneven <88163801+oddneven@users.noreply.github.com>
2021-08-10 12:22:20 +0530

    Create restaurant-management-system.php

---
+++ restaurant-management-system.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+ * Plugin Name:     Restaurant Management System
+ * Plugin URI:      #
+ * Description:     
+ * Author:          oddneven
+ * Author URI:      https://github.com/oddneven
+ * Text Domain:     restaurant-management-system
+ * Version:         0.1.0
+ */
